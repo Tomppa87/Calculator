1abb205f971515f9f2fe9762dacb4125b5266929
Thomas Byrne <byrnesy987@gmail.com>
2025-01-13 14:18:32 +0200

    js operator basic functions, html main divs create

---
+++ script.js
@@ -0,0 +1,14 @@
+// operator functions
+
+let sum = function(num1, num2) {
+    return num1 + num2;    
+}
+let subtract = function(num1, num2) {
+    return num1-num2;
+}
+let multiply = function(num1,num2) {
+    return num1*num2;
+}
+let divide = function(num1, num2) {
+    return num1/num2;
+}
