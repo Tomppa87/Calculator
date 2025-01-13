820b35bdb898b7b2f1ee754398d5176f1399f2c7
Thomas Byrne <byrnesy987@gmail.com>
2025-01-13 15:29:07 +0200

    created number buttons set as flex

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // operator functions
-
 let sum = function(num1, num2) {
     return num1 + num2;    
 }
@@ -11,4 +10,15 @@ let multiply = function(num1,num2) {
 }
 let divide = function(num1, num2) {
     return num1/num2;
+}
+const container = document.getElementsByClassName("container")
+const numContainer = document.getElementById("numberContainer")
+const opContainer = document.getElementsByClassName("operatorContainer")
+
+for (i=0; i<10; i++) {
+    const numBtn = document.createElement("button")
+    const numTxt = document.createTextNode(i+"")
+    numBtn.appendChild(numTxt);
+    console.log(numBtn);
+    numContainer.appendChild(numBtn);
 }
